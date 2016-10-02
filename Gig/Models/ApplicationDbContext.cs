c404d6e372a2c5b83fc8f56de9648340a929a57a
pc <pc@DESKTOP-8D2DGQ2>
2016-10-02 17:14:57 +0200

    Populate data into Genres Table

---
+++ Gig/Models/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Gig.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        /* connection to database */
+        public DbSet<Gig> Gigs { get; set; }
+        public DbSet<Genre>  Genres { get; set; }
+
+
+
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
